8388d8680ee002a9b3e7b6e2c1f97f536426bf41
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-08-28 16:46:44 +0530

    Sarthak | Adds println to debug the failure in pipeline

--- parser/executor/SelectQueryExecutor.go
+++ parser/executor/SelectQueryExecutor.go
@@ -1,23 +1,26 @@
 package executor
 
 import (
+	"fmt"
 	"goselect/parser"
 	"goselect/parser/projection"
 	"io/ioutil"
 )
 
-func ExecuteSelect(query *parser.SelectQuery) [][]interface{} {
+func ExecuteSelect(query *parser.SelectQuery) ([][]interface{}, error) {
 	source := query.Source
 	files, err := ioutil.ReadDir(source.Directory)
 	if err != nil {
-		return nil
+		fmt.Println(err)
+		return nil, err
 	}
 	var rows [][]interface{}
 	for _, file := range files {
+		fmt.Println("file name ", file.Name())
 		fileAttributes := projection.FromFile(file)
 		row := query.Projections.AllExpressions().ExecuteWith(fileAttributes)
 		rows = append(rows, row)
 		//handle recursion
 	}
-	return rows
+	return rows, nil
 }
