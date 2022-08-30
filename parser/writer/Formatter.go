816dbae6f9a59e73cf2eda6659ceb0b513e437ed
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-08-30 13:31:49 +0530

    Sarthak | Separates JsonWriter into a separate file

--- parser/writer/Formatter.go
+++ parser/writer/Formatter.go
@@ -3,59 +3,8 @@ package writer
 import (
 	"goselect/parser/context"
 	"goselect/parser/projection"
-	"strings"
 )
 
 type Formatter interface {
 	Format(projections *projection.Projections, rows [][]context.Value) string
-}
-
-type JsonFormatter struct{}
-
-func NewJsonFormatter() *JsonFormatter {
-	return &JsonFormatter{}
-}
-
-func (jsonFormatter JsonFormatter) Format(projections *projection.Projections, rows [][]context.Value) string {
-	attributes := projections.DisplayableAttributes()
-
-	attributeNameAsString := func(attributeIndex int) string {
-		var value strings.Builder
-		value.WriteString("\"")
-		value.WriteString(attributes[attributeIndex])
-		value.WriteString("\"")
-
-		return value.String()
-	}
-	attributeValueAsString := func(attributeValue context.Value) string {
-		var value strings.Builder
-		value.WriteString("\"")
-		value.WriteString(attributeValue.GetAsString())
-		value.WriteString("\"")
-
-		return value.String()
-	}
-	buildJson := func() string {
-		var json strings.Builder
-		json.WriteString("[")
-
-		for rowIndex, row := range rows {
-			json.WriteString("{")
-			for attributeIndex, attributeValue := range row {
-				json.WriteString(attributeNameAsString(attributeIndex))
-				json.WriteString(" : ")
-				json.WriteString(attributeValueAsString(attributeValue))
-				if attributeIndex != len(row)-1 {
-					json.WriteString(", ")
-				}
-			}
-			json.WriteString("}")
-			if rowIndex != len(rows)-1 {
-				json.WriteString(", ")
-			}
-		}
-		json.WriteString("]")
-		return json.String()
-	}
-	return buildJson()
 }
