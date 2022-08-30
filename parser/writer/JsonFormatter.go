ff886fc7cb31552b9b63642dad35a499736beba6
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-08-30 14:19:28 +0530

    Sarthak | Refactors JsonFormatter

--- parser/writer/JsonFormatter.go
+++ parser/writer/JsonFormatter.go
@@ -13,12 +13,11 @@ func NewJsonFormatter() *JsonFormatter {
 }
 
 func (jsonFormatter JsonFormatter) Format(projections *projection.Projections, rows [][]context.Value) string {
-	attributes := projections.DisplayableAttributes()
 
-	attributeNameAsString := func(attributeIndex int) string {
+	attributeNameAsString := func(attribute string) string {
 		var value strings.Builder
 		value.WriteString("\"")
-		value.WriteString(attributes[attributeIndex])
+		value.WriteString(attribute)
 		value.WriteString("\"")
 
 		return value.String()
@@ -32,26 +31,51 @@ func (jsonFormatter JsonFormatter) Format(projections *projection.Projections, r
 		return value.String()
 	}
 	buildJson := func() string {
-		var json strings.Builder
-		json.WriteString("[")
+		attributes := projections.DisplayableAttributes()
+		var json = new(strings.Builder)
+		jsonFormatter.begin(json)
 
 		for rowIndex, row := range rows {
-			json.WriteString("{")
+			jsonFormatter.beginRow(json)
 			for attributeIndex, attributeValue := range row {
-				json.WriteString(attributeNameAsString(attributeIndex))
-				json.WriteString(" : ")
-				json.WriteString(attributeValueAsString(attributeValue))
+				jsonFormatter.writeAttribute(json, attributeNameAsString(attributes[attributeIndex]), attributeValueAsString(attributeValue))
 				if attributeIndex != len(row)-1 {
-					json.WriteString(", ")
+					jsonFormatter.writeSeparator(json)
 				}
 			}
-			json.WriteString("}")
+			jsonFormatter.closeRow(json)
 			if rowIndex != len(rows)-1 {
-				json.WriteString(", ")
+				jsonFormatter.writeSeparator(json)
 			}
 		}
-		json.WriteString("]")
+		jsonFormatter.end(json)
 		return json.String()
 	}
 	return buildJson()
+}
+
+func (jsonFormatter JsonFormatter) begin(json *strings.Builder) {
+	json.WriteString("[")
+}
+
+func (jsonFormatter JsonFormatter) beginRow(json *strings.Builder) {
+	json.WriteString("{")
+}
+
+func (jsonFormatter JsonFormatter) writeAttribute(json *strings.Builder, attributeName, attributeValue string) {
+	json.WriteString(attributeName)
+	json.WriteString(" : ")
+	json.WriteString(attributeValue)
+}
+
+func (jsonFormatter JsonFormatter) writeSeparator(json *strings.Builder) {
+	json.WriteString(", ")
+}
+
+func (jsonFormatter JsonFormatter) closeRow(json *strings.Builder) {
+	json.WriteString("}")
+}
+
+func (jsonFormatter JsonFormatter) end(json *strings.Builder) {
+	json.WriteString("]")
 }
