79cbea49cf7e59a64a5e942a674ff4ca55e9d4bd
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-08-30 14:07:49 +0530

    Sarthak | Introduces HtmlFormatter

---
+++ parser/writer/HtmlFormatter.go
@@ -0,0 +1,89 @@
+package writer
+
+import (
+	"goselect/parser/context"
+	"goselect/parser/projection"
+	"strings"
+)
+
+type HtmlFormatter struct{}
+
+func NewHtmlFormatter() *HtmlFormatter {
+	return &HtmlFormatter{}
+}
+
+func (htmlFormatter HtmlFormatter) Format(projections *projection.Projections, rows [][]context.Value) string {
+	var result = new(strings.Builder)
+	htmlFormatter.beginHtml(result)
+	htmlFormatter.beginBody(result)
+	htmlFormatter.beginTable(result)
+	htmlFormatter.beginTableHeader(result, projections)
+	htmlFormatter.beginTableContent(result, rows)
+	htmlFormatter.closeTable(result)
+	htmlFormatter.closeBody(result)
+	htmlFormatter.closeHtml(result)
+
+	return result.String()
+}
+
+func (htmlFormatter HtmlFormatter) beginHtml(html *strings.Builder) {
+	html.WriteString("<html>")
+}
+
+func (htmlFormatter HtmlFormatter) beginBody(html *strings.Builder) {
+	html.WriteString("<body>")
+}
+
+func (htmlFormatter HtmlFormatter) beginTable(html *strings.Builder) {
+	html.WriteString("<table style=\"width:100%; border: 1px solid black\">")
+}
+
+func (htmlFormatter HtmlFormatter) beginTableHeader(html *strings.Builder, projections *projection.Projections) {
+	htmlFormatter.beginRow(html)
+	for _, attribute := range projections.DisplayableAttributes() {
+		htmlFormatter.writeColumnHeader(html, attribute)
+	}
+	htmlFormatter.closeRow(html)
+}
+
+func (htmlFormatter HtmlFormatter) beginTableContent(html *strings.Builder, queryResults [][]context.Value) {
+	for _, row := range queryResults {
+		htmlFormatter.beginRow(html)
+		for _, attribute := range row {
+			htmlFormatter.writeColumnContent(html, attribute.GetAsString())
+		}
+		htmlFormatter.closeRow(html)
+	}
+}
+
+func (htmlFormatter HtmlFormatter) beginRow(html *strings.Builder) {
+	html.WriteString("<tr>")
+}
+
+func (htmlFormatter HtmlFormatter) writeColumnHeader(html *strings.Builder, column string) {
+	html.WriteString("<th style=\"border: 1px solid black\">")
+	html.WriteString(column)
+	html.WriteString("</th>")
+}
+
+func (htmlFormatter HtmlFormatter) writeColumnContent(html *strings.Builder, column string) {
+	html.WriteString("<td style=\"border: 1px solid black\">")
+	html.WriteString(column)
+	html.WriteString("</td>")
+}
+
+func (htmlFormatter HtmlFormatter) closeRow(html *strings.Builder) {
+	html.WriteString("</tr>")
+}
+
+func (htmlFormatter HtmlFormatter) closeTable(html *strings.Builder) {
+	html.WriteString("</table>")
+}
+
+func (htmlFormatter HtmlFormatter) closeBody(html *strings.Builder) {
+	html.WriteString("</body>")
+}
+
+func (htmlFormatter HtmlFormatter) closeHtml(html *strings.Builder) {
+	html.WriteString("</html>")
+}
---
+++ parser/writer/HtmlFormatter_test.go
@@ -0,0 +1,28 @@
+package writer
+
+import (
+	"goselect/parser"
+	"goselect/parser/context"
+	"goselect/parser/executor"
+	"testing"
+)
+
+func TestHtmlFormatter(t *testing.T) {
+	newContext := context.NewContext(context.NewFunctions(), context.NewAttributes())
+	aParser, err := parser.NewParser("select lower(name), contains(lower(name), 'log') from ../resources/TestResultsWithProjections/multi order by 1", newContext)
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	selectQuery, err := aParser.Parse()
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	queryResults, _ := executor.NewSelectQueryExecutor(selectQuery, newContext).Execute()
+
+	html := NewHtmlFormatter().Format(selectQuery.Projections, queryResults)
+	expected := "<html><body><table style=\"width:100%; border: 1px solid black\"><tr><th style=\"border: 1px solid black\">lower(name)</th><th style=\"border: 1px solid black\">contains(lower(name),log)</th></tr><tr><td style=\"border: 1px solid black\">testresultswithprojections_a.log</td><td style=\"border: 1px solid black\">Y</td></tr><tr><td style=\"border: 1px solid black\">testresultswithprojections_b.log</td><td style=\"border: 1px solid black\">Y</td></tr><tr><td style=\"border: 1px solid black\">testresultswithprojections_c.txt</td><td style=\"border: 1px solid black\">N</td></tr><tr><td style=\"border: 1px solid black\">testresultswithprojections_d.txt</td><td style=\"border: 1px solid black\">N</td></tr></table></body></html>"
+
+	if expected != html {
+		t.Fatalf("Expected html formatter to format %v, received %v", expected, html)
+	}
+}
