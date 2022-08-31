2fcf32154b1b86b9bdd852f7b72f991706acc22c
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-08-31 18:23:29 +0530

    Sarthak | Adds tests for average at the level of functions

--- parser/context/AggregrationFunctionBlock_test.go
+++ parser/context/AggregrationFunctionBlock_test.go
@@ -12,8 +12,32 @@ func TestCount(t *testing.T) {
 	state, _ = allFunctions.ExecuteAggregate("count", state, StringValue("arg1"))
 	state, _ = allFunctions.ExecuteAggregate("count", state, StringValue("arg1"))
 
-	actualValue := state.Initial.GetAsString()
+	actualValue := allFunctions.FinalState("count", state).GetAsString()
 	if actualValue != "3" {
 		t.Fatalf("Expected count to be %v, received %v", "3", actualValue)
+	}
+}
+
+func TestAverage(t *testing.T) {
+	allFunctions := NewFunctions()
+	initialState := allFunctions.InitialState("average")
+
+	state, _ := allFunctions.ExecuteAggregate("average", initialState, IntValue(10))
+	state, _ = allFunctions.ExecuteAggregate("average", state, IntValue(11))
+	state, _ = allFunctions.ExecuteAggregate("average", state, IntValue(11))
+
+	actualValue := allFunctions.FinalState("average", state).GetAsString()
+	if actualValue != "10.67" {
+		t.Fatalf("Expected count to be %v, received %v", "10.67", actualValue)
+	}
+}
+
+func TestAverageGivenANonNumericParameter(t *testing.T) {
+	allFunctions := NewFunctions()
+	initialState := allFunctions.InitialState("average")
+
+	_, err := allFunctions.ExecuteAggregate("average", initialState, StringValue("a"))
+	if err == nil {
+		t.Fatalf("Expected an error on running average with a non numeric parameter")
 	}
 }
