579ed80850945fa787f4700f36cc68bd2ea359ad
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-09-01 20:32:42 +0530

    Sarthak | Introduces Where as an abstraction

--- parser/where/Where.go
+++ parser/where/Where.go
@@ -1,4 +1,4 @@
-package projection
+package where
 
 import (
 	"errors"
@@ -8,76 +8,62 @@ import (
 	"goselect/parser/tokenizer"
 )
 
-type Projections struct {
+type Where struct {
 	expressions expression.Expressions
 }
 
-func NewProjections(
+func NewWhere(
 	tokenIterator *tokenizer.TokenIterator,
 	context *context.ParsingApplicationContext,
-) (*Projections, error) {
-	if expressions, err := all(tokenIterator, context); err != nil {
+) (*Where, error) {
+	if expressions, isWhereSpecified, err := all(tokenIterator, context); err != nil {
 		return nil, err
 	} else {
-		if expressions.Count() == 0 {
-			return nil, errors.New(messages.ErrorMessageExpectedExpressionInProjection)
+		if isWhereSpecified && expressions.Count() == 0 {
+			return nil, errors.New(messages.ErrorMessageExpectedExpressionInWhere)
 		}
-		return &Projections{expressions: expressions}, nil
+		return &Where{expressions: expressions}, nil
 	}
 }
 
-func (projections Projections) Count() int {
-	return projections.expressions.Count()
+func (where Where) Display() string {
+	if attributes := where.expressions.DisplayableAttributes(); len(attributes) >= 1 {
+		return attributes[0]
+	}
+	return ""
 }
 
-func (projections Projections) DisplayableAttributes() []string {
-	return projections.expressions.DisplayableAttributes()
-}
-
-func (projections Projections) EvaluateWith(
+func (where Where) EvaluateWith(
 	fileAttributes *context.FileAttributes,
 	functions *context.AllFunctions,
 ) ([]context.Value, []bool, []*expression.Expression, error) {
-	return projections.expressions.EvaluateWith(fileAttributes, functions)
+	return where.expressions.EvaluateWith(fileAttributes, functions)
 }
 
-/*
-projection:  attributes Or functions Or expressions
-attributes:  name, size etc
-functions: 	 min(size), lower(name), min(Count(size)) etc
-expressions: add(..), mul(..), gt(..)
-*/
 func all(
 	tokenIterator *tokenizer.TokenIterator,
 	ctx *context.ParsingApplicationContext,
-) (expression.Expressions, error) {
-	var expressions []*expression.Expression
-	var expectComma bool
+) (expression.Expressions, bool, error) {
 
-	for tokenIterator.HasNext() && !tokenIterator.Peek().Equals("from") {
+	var expressions []*expression.Expression
+	if tokenIterator.HasNext() && !tokenIterator.Peek().Equals("where") {
+		return expression.Expressions{}, false, nil
+	}
+	if tokenIterator.HasNext() && tokenIterator.Peek().Equals("where") {
+		tokenIterator.Next()
+	}
+	if tokenIterator.HasNext() && !tokenIterator.Peek().Equals("order") {
 		token := tokenIterator.Next()
 		switch {
-		case expectComma:
-			if !token.Equals(",") {
-				return expression.Expressions{}, errors.New(messages.ErrorMessageMissingCommaProjection)
-			}
-			expectComma = false
-		case context.IsAWildcardAttribute(token.TokenValue):
-			expressions = append(expressions, expression.ExpressionsWithAttributes(context.AttributesOnWildcard())...)
-			expectComma = true
-		case ctx.IsASupportedAttribute(token.TokenValue):
-			expressions = append(expressions, expression.ExpressionWithAttribute(token.TokenValue))
-			expectComma = true
 		case ctx.IsASupportedFunction(token.TokenValue):
 			if function, err := function(token, tokenIterator, ctx); err != nil {
-				return expression.Expressions{}, err
+				return expression.Expressions{}, true, err
 			} else {
 				expressions = append(expressions, expression.ExpressionWithFunctionInstance(function))
 			}
-			expectComma = true
 		}
 	}
-	return expression.Expressions{Expressions: expressions}, nil
+	return expression.Expressions{Expressions: expressions}, true, nil
 }
 
 func function(
@@ -85,19 +71,13 @@ func function(
 	tokenIterator *tokenizer.TokenIterator,
 	ctx *context.ParsingApplicationContext,
 ) (*expression.FunctionInstance, error) {
-	var parseFunction func(functionNameToken tokenizer.Token) (*expression.FunctionInstance, error)
 
-	aggregateFunctionStateOrNil := func(fn string) *context.FunctionState {
-		if ctx.IsAnAggregateFunction(fn) {
-			return ctx.InitialState(fn)
-		}
-		return nil
-	}
+	var parseFunction func(functionNameToken tokenizer.Token) (*expression.FunctionInstance, error)
 	parseFunction = func(functionNameToken tokenizer.Token) (*expression.FunctionInstance, error) {
 		var functionArgs []*expression.Expression
 		expectOpeningParentheses := true
 
-		for tokenIterator.HasNext() && !tokenIterator.Peek().Equals("from") {
+		for tokenIterator.HasNext() && !tokenIterator.Peek().Equals("order") {
 			token := tokenIterator.Next()
 			switch {
 			case expectOpeningParentheses:
@@ -109,7 +89,7 @@ func function(
 				return expression.FunctionInstanceWith(
 					functionNameToken.TokenValue,
 					functionArgs,
-					aggregateFunctionStateOrNil(functionNameToken.TokenValue),
+					nil,
 				), nil
 			case ctx.IsASupportedFunction(token.TokenValue):
 				fn, err := parseFunction(token)
@@ -132,7 +112,7 @@ func function(
 	if fn, err := parseFunction(functionNameToken); err != nil {
 		return nil, err
 	} else if fn == nil {
-		return nil, errors.New(messages.ErrorMessageInvalidProjection)
+		return nil, errors.New(messages.ErrorMessageInvalidWhere)
 	} else {
 		return fn, nil
 	}
---
+++ parser/where/Where_test.go
@@ -0,0 +1,53 @@
+package where
+
+import (
+	"goselect/parser/context"
+	"goselect/parser/tokenizer"
+	"testing"
+)
+
+func TestWhereWithoutAnyWhereClause(t *testing.T) {
+	tokens := tokenizer.NewEmptyTokens()
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "order"))
+	tokens.Add(tokenizer.NewToken(tokenizer.OpeningParentheses, "by"))
+
+	where, _ := NewWhere(tokens.Iterator(), context.NewContext(context.NewFunctions(), context.NewAttributes()))
+	expected := ""
+
+	if expected != where.Display() {
+		t.Fatalf("Expected where clause to be blank %v, received %v", "", where.Display())
+	}
+}
+
+func TestWhere(t *testing.T) {
+	tokens := tokenizer.NewEmptyTokens()
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "where"))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "contains"))
+	tokens.Add(tokenizer.NewToken(tokenizer.OpeningParentheses, "("))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "name"))
+	tokens.Add(tokenizer.NewToken(tokenizer.Comma, ","))
+	tokens.Add(tokenizer.NewToken(tokenizer.Comma, "log"))
+	tokens.Add(tokenizer.NewToken(tokenizer.ClosingParentheses, ")"))
+
+	where, _ := NewWhere(tokens.Iterator(), context.NewContext(context.NewFunctions(), context.NewAttributes()))
+	expected := "contains(name,log)"
+
+	if expected != where.Display() {
+		t.Fatalf("Expected where clause to be %v, received %v", expected, where.Display())
+	}
+}
+
+func TestThrowsAnErrorWithInvalidParentheses(t *testing.T) {
+	tokens := tokenizer.NewEmptyTokens()
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "where"))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "contains"))
+	tokens.Add(tokenizer.NewToken(tokenizer.OpeningParentheses, "("))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "name"))
+	tokens.Add(tokenizer.NewToken(tokenizer.Comma, ","))
+	tokens.Add(tokenizer.NewToken(tokenizer.Comma, "log"))
+
+	_, err := NewWhere(tokens.Iterator(), context.NewContext(context.NewFunctions(), context.NewAttributes()))
+	if err == nil {
+		t.Fatalf("Expected an error for invalid where clause but received none")
+	}
+}
