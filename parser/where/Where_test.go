53f3670ad8a40a72a7811fcc814b61def7b6c2d8
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-09-01 21:28:07 +0530

    Sarthak | Introduces Eq in where clause in where test

--- parser/where/Where_test.go
+++ parser/where/Where_test.go
@@ -82,7 +82,7 @@ func TestEvaluatesWhereClauseWithContains(t *testing.T) {
 	}
 }
 
-func TestEvaluatesWhereClauseWithContainsq3(t *testing.T) {
+func TestEvaluatesWhereClauseWithSubstrWithAnError(t *testing.T) {
 	tokens := tokenizer.NewEmptyTokens()
 	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "where"))
 	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "substr"))
@@ -100,5 +100,31 @@ func TestEvaluatesWhereClauseWithContainsq3(t *testing.T) {
 
 	if err == nil {
 		t.Fatalf("Expected an error while evaluating a where clause that does not return a boolean value")
+	}
+}
+
+func TestEvaluatesWhereClauseEq1(t *testing.T) {
+	tokens := tokenizer.NewEmptyTokens()
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "where"))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "eq"))
+	tokens.Add(tokenizer.NewToken(tokenizer.OpeningParentheses, "("))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "substr"))
+	tokens.Add(tokenizer.NewToken(tokenizer.OpeningParentheses, "("))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "Dummylog"))
+	tokens.Add(tokenizer.NewToken(tokenizer.Comma, ","))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "1"))
+	tokens.Add(tokenizer.NewToken(tokenizer.Comma, ","))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "3"))
+	tokens.Add(tokenizer.NewToken(tokenizer.ClosingParentheses, ")"))
+	tokens.Add(tokenizer.NewToken(tokenizer.Comma, ","))
+	tokens.Add(tokenizer.NewToken(tokenizer.RawString, "umm"))
+	tokens.Add(tokenizer.NewToken(tokenizer.ClosingParentheses, ")"))
+
+	functions := context.NewFunctions()
+	where, _ := NewWhere(tokens.Iterator(), context.NewContext(functions, context.NewAttributes()))
+	value, _ := where.EvaluateWith(nil, functions)
+
+	if value != true {
+		t.Fatalf("Expected where clause to evaluate to true but it did not")
 	}
 }
