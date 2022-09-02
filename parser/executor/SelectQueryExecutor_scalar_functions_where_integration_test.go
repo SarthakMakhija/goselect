abfa13e10bfd2af4e9e52e4ed5569aaf3c9c8c30
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-09-02 12:42:09 +0530

    Sarthak | Adds a few integration tests for select query with where clause

---
+++ parser/executor/SelectQueryExecutor_scalar_functions_where_integration_test.go
@@ -0,0 +1,82 @@
+package executor
+
+import (
+	"goselect/parser"
+	"goselect/parser/context"
+	"testing"
+)
+
+func TestResultsWithAWhereClause1(t *testing.T) {
+	newContext := context.NewContext(context.NewFunctions(), context.NewAttributes())
+	aParser, err := parser.NewParser("select lower(name) from ../resources/TestResultsWithProjections/multi where eq(ext, .log) order by 1", newContext)
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	selectQuery, err := aParser.Parse()
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	queryResults, _ := NewSelectQueryExecutor(selectQuery, newContext).Execute()
+	expected := [][]context.Value{
+		{context.StringValue("testresultswithprojections_a.log")},
+		{context.StringValue("testresultswithprojections_b.log")},
+	}
+	assertMatch(t, expected, queryResults)
+}
+
+func TestResultsWithAWhereClause2(t *testing.T) {
+	newContext := context.NewContext(context.NewFunctions(), context.NewAttributes())
+	aParser, err := parser.NewParser("select lower(name) from ../resources/TestResultsWithProjections/multi where eq(add(2,3), 5) order by 1", newContext)
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	selectQuery, err := aParser.Parse()
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	queryResults, _ := NewSelectQueryExecutor(selectQuery, newContext).Execute()
+	expected := [][]context.Value{
+		{context.StringValue("testresultswithprojections_a.log")},
+		{context.StringValue("testresultswithprojections_b.log")},
+		{context.StringValue("testresultswithprojections_c.txt")},
+		{context.StringValue("testresultswithprojections_d.txt")},
+	}
+	assertMatch(t, expected, queryResults)
+}
+
+func TestResultsWithAWhereClause3(t *testing.T) {
+	newContext := context.NewContext(context.NewFunctions(), context.NewAttributes())
+	aParser, err := parser.NewParser("select lower(name) from ../resources/TestResultsWithProjections/multi where contains(lower(name), a.log) order by 1", newContext)
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	selectQuery, err := aParser.Parse()
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	queryResults, _ := NewSelectQueryExecutor(selectQuery, newContext).Execute()
+	expected := [][]context.Value{
+		{context.StringValue("testresultswithprojections_a.log")},
+	}
+	assertMatch(t, expected, queryResults)
+}
+
+func TestResultsWithAWhereClause4(t *testing.T) {
+	newContext := context.NewContext(context.NewFunctions(), context.NewAttributes())
+	aParser, err := parser.NewParser("select lower(name) from ../resources/TestResultsWithProjections/multi where eq(lower(substr(name, 0, 3)), test) order by 1", newContext)
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	selectQuery, err := aParser.Parse()
+	if err != nil {
+		t.Fatalf("error is %v", err)
+	}
+	queryResults, _ := NewSelectQueryExecutor(selectQuery, newContext).Execute()
+	expected := [][]context.Value{
+		{context.StringValue("testresultswithprojections_a.log")},
+		{context.StringValue("testresultswithprojections_b.log")},
+		{context.StringValue("testresultswithprojections_c.txt")},
+		{context.StringValue("testresultswithprojections_d.txt")},
+	}
+	assertMatch(t, expected, queryResults)
+}
