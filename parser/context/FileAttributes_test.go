234521f6bf44d1daae0b2a48b6a6111f1a2d6035
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-09-05 12:23:30 +0530

    Sarthak | Adds tests for FileAttributes

---
+++ parser/context/FileAttributes_test.go
@@ -0,0 +1,194 @@
+package context
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestFileName(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/single/TestResultsWithProjections_A.txt")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/single/", file, context)
+	name := fileAttributes.Get(AttributeName).GetAsString()
+
+	if name != "TestResultsWithProjections_A.txt" {
+		t.Fatalf("Expected file name to be %v, received %v", "TestResultsWithProjections_A.txt", name)
+	}
+}
+
+func TestFileExtension(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/single/TestResultsWithProjections_A.txt")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/single/", file, context)
+	extension := fileAttributes.Get(AttributeExtension).GetAsString()
+
+	if extension != ".txt" {
+		t.Fatalf("Expected file extension to be %v, received %v", ".txt", extension)
+	}
+}
+
+func TestFileBaseName(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/single/TestResultsWithProjections_A.txt")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/single/", file, context)
+	baseName := fileAttributes.Get(AttributeBaseName).GetAsString()
+
+	if baseName != "TestResultsWithProjections_A" {
+		t.Fatalf("Expected file baseName to be %v, received %v", "TestResultsWithProjections_A", baseName)
+	}
+}
+
+func TestFileSize(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/single/TestResultsWithProjections_A.txt")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/single/", file, context)
+	size := fileAttributes.Get(AttributeFormattedSize).GetAsString()
+
+	if size != "58 B" {
+		t.Fatalf("Expected file baseName to be %v, received %v", "58 B", size)
+	}
+}
+
+func TestFileType1(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/single/TestResultsWithProjections_A.txt")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/single/", file, context)
+	isFile, _ := fileAttributes.Get(AttributeNameIsFile).GetBoolean()
+
+	if isFile != true {
+		t.Fatalf("Expected TestResultsWithProjections_A to be a file but was not")
+	}
+}
+
+func TestFileType2(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/single/TestResultsWithProjections_A.txt")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/single/", file, context)
+	isDirectory, _ := fileAttributes.Get(AttributeNameIsDir).GetBoolean()
+
+	if isDirectory != false {
+		t.Fatalf("Expected TestResultsWithProjections_A to not be a directory but was")
+	}
+}
+
+func TestFileType3(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/single/TestResultsWithProjections_A.txt")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/single/", file, context)
+	isHidden, _ := fileAttributes.Get(AttributeNameIsHidden).GetBoolean()
+
+	if isHidden != false {
+		t.Fatalf("Expected TestResultsWithProjections_A to not be hidden but was")
+	}
+}
+
+func TestFileType4(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/empty/Empty.log")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/empty/", file, context)
+	isEmpty, _ := fileAttributes.Get(AttributeNameIsEmpty).GetBoolean()
+
+	if isEmpty != true {
+		t.Fatalf("Expected Empty.log to be empty but was not")
+	}
+}
+
+func TestFilePermission(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/empty/Empty.log")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/empty/", file, context)
+	permission := fileAttributes.Get(AttributePermission).GetAsString()
+	expected := "-rw-r--r--"
+
+	if permission != expected {
+		t.Fatalf("Expected permission to be %v, received %v", expected, permission)
+	}
+}
+
+func TestFilePermissionForUsers(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/empty/Empty.log")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/empty/", file, context)
+
+	userRead, _ := fileAttributes.Get(AttributeUserRead).GetBoolean()
+	userWrite, _ := fileAttributes.Get(AttributeUserWrite).GetBoolean()
+	userExecute, _ := fileAttributes.Get(AttributeUserExecute).GetBoolean()
+
+	expected := []bool{true, true, false}
+	received := []bool{userRead, userWrite, userExecute}
+
+	if !reflect.DeepEqual(expected, received) {
+		t.Fatalf("Expected permissions for user to be %v, received %v", expected, received)
+	}
+}
+
+func TestFilePermissionForGroup(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/empty/Empty.log")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/empty/", file, context)
+
+	groupRead, _ := fileAttributes.Get(AttributeGroupRead).GetBoolean()
+	groupWrite, _ := fileAttributes.Get(AttributeGroupWrite).GetBoolean()
+	groupExecute, _ := fileAttributes.Get(AttributeGroupExecute).GetBoolean()
+
+	expected := []bool{true, false, false}
+	received := []bool{groupRead, groupWrite, groupExecute}
+
+	if !reflect.DeepEqual(expected, received) {
+		t.Fatalf("Expected permissions for group to be %v, received %v", expected, received)
+	}
+}
+
+func TestFilePermissionForOthers(t *testing.T) {
+	file, err := os.Stat("../resources/TestResultsWithProjections/empty/Empty.log")
+	if err != nil {
+		panic(err)
+	}
+	context := NewContext(nil, NewAttributes())
+	fileAttributes := ToFileAttributes("../resource/TestResultsWithProjections/empty/", file, context)
+
+	othersRead, _ := fileAttributes.Get(AttributeOthersRead).GetBoolean()
+	othersWrite, _ := fileAttributes.Get(AttributeOthersWrite).GetBoolean()
+	othersExecute, _ := fileAttributes.Get(AttributeOthersExecute).GetBoolean()
+
+	expected := []bool{true, false, false}
+	received := []bool{othersRead, othersWrite, othersExecute}
+
+	if !reflect.DeepEqual(expected, received) {
+		t.Fatalf("Expected permissions for others to be %v, received %v", expected, received)
+	}
+}
