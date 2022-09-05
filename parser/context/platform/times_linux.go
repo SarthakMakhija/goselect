b968c24e657806e2af3d5bcc989511feffb2b85b
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-09-05 15:47:08 +0530

    Sarthak | Attempts to correct the failing build for linux

--- parser/context/platform/times_linux.go
+++ parser/context/platform/times_linux.go
@@ -9,6 +9,10 @@ import (
 	"time"
 )
 
+type CreatedTime = time.Time
+type ModifiedTime = time.Time
+type AccessTime = time.Time
+
 func FileTimes(file fs.FileInfo) (CreatedTime, ModifiedTime, AccessTime) {
 	toTime := func(ts syscall.Timespec) time.Time {
 		return time.Unix(ts.Sec, ts.Nsec)
