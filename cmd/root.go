b3e7c3b92a2f5222d695e8a58d8fce3f176bfb23
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-09-11 12:11:51 +0530

    Sarthak | Disables completion command

--- cmd/root.go
+++ cmd/root.go
@@ -29,6 +29,7 @@ Features that are different from SQL:
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
