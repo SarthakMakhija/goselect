47f248382dda934c76c1d0d06e27597978fb44a3
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-09-26 18:02:06 +0530

    Sarthak | Refactors listFunctions

--- cmd/listFunctions.go
+++ cmd/listFunctions.go
@@ -1,70 +1,77 @@
 package cmd
 
 import (
+	"bytes"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 	"goselect/parser/context"
-	"os"
 	"sort"
 	"strings"
 )
 
-var listFunctionsCmd = &cobra.Command{
-	Use:     "listFunctions",
-	Aliases: []string{"functions", "fns"},
-	Short:   "List all the functions supported by goselect",
-	Long:    `List all the functions along with their aliases supported by goselect`,
-	Example: `
+func newListFunctionsCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:     "listFunctions",
+		Aliases: []string{"functions", "fns"},
+		Short:   "List all the functions supported by goselect",
+		Long:    `List all the functions along with their aliases supported by goselect`,
+		Example: `
 1. goselect listFunctions --sorted=true
 2. goselect functions --sorted=true
 3. goselect fns --sorted=true
 `,
-	Run: func(cmd *cobra.Command, args []string) {
-		tableWriter := table.NewWriter()
-		tableWriter.SetOutputMirror(os.Stdout)
-		tableWriter.SetStyle(table.StyleColoredBlackOnCyanWhite)
-		tableWriter.Style().Options.SeparateColumns = true
+		Run: func(cmd *cobra.Command, args []string) {
+			buffer := new(bytes.Buffer)
 
-		asString := func(aliases []string) string {
-			return strings.Join(aliases, ", ")
-		}
-		printHeader := func() {
-			tableWriter.AppendHeader(table.Row{"Function", "Aliases"})
-		}
-		printFunction := func(function string, aliases []string) {
-			tableWriter.AppendRow(table.Row{function, asString(aliases)})
-		}
-		printFunctions := func(aliasesByFunction map[string][]string) {
-			for function, aliases := range aliasesByFunction {
-				printFunction(function, aliases)
-			}
-		}
-		sortFunctions := func(aliasesByFunction map[string][]string) []string {
-			functions := make([]string, 0, len(aliasesByFunction))
-			for function := range aliasesByFunction {
-				functions = append(functions, function)
-			}
-			sort.Strings(functions)
-			return functions
-		}
+			tableWriter := table.NewWriter()
+			tableWriter.SetOutputMirror(buffer)
+			tableWriter.SetStyle(table.StyleColoredBlackOnCyanWhite)
+			tableWriter.Style().Options.SeparateColumns = true
 
-		isSorted, _ := cmd.Flags().GetBool("sorted")
-		aliasesByFunction := context.NewFunctions().AllFunctionsWithAliases()
-		printHeader()
-
-		if !isSorted {
-			printFunctions(aliasesByFunction)
-			tableWriter.Render()
-		} else {
-			for _, function := range sortFunctions(aliasesByFunction) {
-				printFunction(function, aliasesByFunction[function])
+			asString := func(aliases []string) string {
+				return strings.Join(aliases, ", ")
 			}
-			tableWriter.Render()
-		}
-	},
+			appendHeader := func() {
+				tableWriter.AppendHeader(table.Row{"Function", "Aliases"})
+			}
+			appendFunction := func(function string, aliases []string) {
+				tableWriter.AppendRow(table.Row{function, asString(aliases)})
+			}
+			appendFunctions := func(aliasesByFunction map[string][]string) {
+				for function, aliases := range aliasesByFunction {
+					appendFunction(function, aliases)
+				}
+			}
+			sortFunctions := func(aliasesByFunction map[string][]string) []string {
+				functions := make([]string, 0, len(aliasesByFunction))
+				for function := range aliasesByFunction {
+					functions = append(functions, function)
+				}
+				sort.Strings(functions)
+				return functions
+			}
+
+			isSorted, _ := cmd.Flags().GetBool("sorted")
+			aliasesByFunction := context.NewFunctions().AllFunctionsWithAliases()
+			appendHeader()
+
+			if !isSorted {
+				appendFunctions(aliasesByFunction)
+				tableWriter.Render()
+				cmd.Print(buffer.String())
+			} else {
+				for _, function := range sortFunctions(aliasesByFunction) {
+					appendFunction(function, aliasesByFunction[function])
+				}
+				tableWriter.Render()
+				cmd.Print(buffer.String())
+			}
+		},
+	}
 }
 
 func init() {
+	listFunctionsCmd := newListFunctionsCommand()
 	rootCmd.AddCommand(listFunctionsCmd)
 	listFunctionsCmd.PersistentFlags().Bool("sorted", true, "display the functions in sorted order. Use --sorted=true or --sorted=false")
 }
---
+++ cmd/listFunctions_test.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"bytes"
+	"goselect/parser/context"
+	"strings"
+	"testing"
+)
+
+func TestFunctionsUnsorted(t *testing.T) {
+	rootCmd.SetArgs([]string{"listFunctions"})
+	buffer := new(bytes.Buffer)
+	rootCmd.SetOut(buffer)
+
+	_ = rootCmd.Execute()
+	contents := buffer.String()
+
+	aliasesByFunction := context.NewFunctions().AllFunctionsWithAliases()
+	for function, aliases := range aliasesByFunction {
+		if !strings.Contains(contents, function) {
+			t.Fatalf("Expected function %v to be contained in the supported functions but was not, received %v", function, contents)
+		}
+		for _, alias := range aliases {
+			if !strings.Contains(contents, alias) {
+				t.Fatalf("Expected alias %v to be contained in the supported functions but was not, received %v", alias, contents)
+			}
+		}
+	}
+}
+
+func TestFunctionsSorted(t *testing.T) {
+	rootCmd.SetArgs([]string{"listFunctions", "--sorted", "true"})
+	buffer := new(bytes.Buffer)
+	rootCmd.SetOut(buffer)
+
+	_ = rootCmd.Execute()
+	contents := buffer.String()
+
+	aliasesByFunction := context.NewFunctions().AllFunctionsWithAliases()
+	for function, aliases := range aliasesByFunction {
+		if !strings.Contains(contents, function) {
+			t.Fatalf("Expected function %v to be contained in the supported functions but was not, received %v", function, contents)
+		}
+		for _, alias := range aliases {
+			if !strings.Contains(contents, alias) {
+				t.Fatalf("Expected alias %v to be contained in the supported functions but was not, received %v", alias, contents)
+			}
+		}
+	}
+}
+
+func TestFunctionsUnSortedWithInvalidValue(t *testing.T) {
+	rootCmd.SetArgs([]string{"listFunctions", "--sorted", "unknown"})
+	buffer := new(bytes.Buffer)
+	rootCmd.SetOut(buffer)
+
+	_ = rootCmd.Execute()
+	contents := buffer.String()
+
+	aliasesByFunction := context.NewFunctions().AllFunctionsWithAliases()
+	for function, aliases := range aliasesByFunction {
+		if !strings.Contains(contents, function) {
+			t.Fatalf("Expected function %v to be contained in the supported functions but was not, received %v", function, contents)
+		}
+		for _, alias := range aliases {
+			if !strings.Contains(contents, alias) {
+				t.Fatalf("Expected alias %v to be contained in the supported functions but was not, received %v", alias, contents)
+			}
+		}
+	}
+}
