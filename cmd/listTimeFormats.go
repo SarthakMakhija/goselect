5e0bf5da8c8cc17ffbf15df3b2ce0e41adadba77
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-09-26 17:42:56 +0530

    Sarthak | Refactors listTimeFormats

--- cmd/listTimeFormats.go
+++ cmd/listTimeFormats.go
@@ -1,65 +1,72 @@
 package cmd
 
 import (
+	"bytes"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 	"goselect/parser/context"
-	"os"
 	"sort"
 )
 
-var listTimeFormatsCmd = &cobra.Command{
-	Use:     "listTimeFormats",
-	Aliases: []string{"formats", "fmts"},
-	Short:   "List the date/time formats supported by goselect",
-	Example: `
+func newListTimeFormatsCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:     "listTimeFormats",
+		Aliases: []string{"formats", "fmts"},
+		Short:   "List the date/time formats supported by goselect",
+		Example: `
 1. goselect listTimeFormats --sorted=true
 2. goselect formats --sorted=true
 3. goselect fmts --sorted=true
 `,
-	Run: func(cmd *cobra.Command, args []string) {
-		tableWriter := table.NewWriter()
-		tableWriter.SetOutputMirror(os.Stdout)
-		tableWriter.SetStyle(table.StyleColoredBlackOnCyanWhite)
-		tableWriter.Style().Options.SeparateColumns = true
+		Run: func(cmd *cobra.Command, args []string) {
+			buffer := new(bytes.Buffer)
 
-		printHeader := func() {
-			tableWriter.AppendHeader(table.Row{"Format", "Id"})
-		}
-		printFormat := func(format context.FormatDefinition) {
-			tableWriter.AppendRow(table.Row{format.Format, format.Id})
-		}
-		printFormats := func(formatDefinitions map[string]context.FormatDefinition) {
-			for _, definition := range formatDefinitions {
-				printFormat(definition)
-			}
-		}
-		sortFormats := func(formatDefinitions map[string]context.FormatDefinition) []string {
-			formats := make([]string, 0, len(formatDefinitions))
-			for format := range formatDefinitions {
-				formats = append(formats, format)
-			}
-			sort.Strings(formats)
-			return formats
-		}
+			tableWriter := table.NewWriter()
+			tableWriter.SetOutputMirror(buffer)
+			tableWriter.SetStyle(table.StyleColoredBlackOnCyanWhite)
+			tableWriter.Style().Options.SeparateColumns = true
 
-		formatDefinitionById := context.SupportedFormats()
-		isSorted, _ := cmd.Flags().GetBool("sorted")
-		printHeader()
-
-		if !isSorted {
-			printFormats(formatDefinitionById)
-			tableWriter.Render()
-		} else {
-			for _, format := range sortFormats(formatDefinitionById) {
-				printFormat(formatDefinitionById[format])
+			printHeader := func() {
+				tableWriter.AppendHeader(table.Row{"Format", "Id"})
 			}
-			tableWriter.Render()
-		}
-	},
+			printFormat := func(format context.FormatDefinition) {
+				tableWriter.AppendRow(table.Row{format.Format, format.Id})
+			}
+			printFormats := func(formatDefinitions map[string]context.FormatDefinition) {
+				for _, definition := range formatDefinitions {
+					printFormat(definition)
+				}
+			}
+			sortFormats := func(formatDefinitions map[string]context.FormatDefinition) []string {
+				formats := make([]string, 0, len(formatDefinitions))
+				for format := range formatDefinitions {
+					formats = append(formats, format)
+				}
+				sort.Strings(formats)
+				return formats
+			}
+
+			formatDefinitionById := context.SupportedFormats()
+			isSorted, _ := cmd.Flags().GetBool("sorted")
+			printHeader()
+
+			if !isSorted {
+				printFormats(formatDefinitionById)
+				tableWriter.Render()
+				cmd.Print(buffer.String())
+			} else {
+				for _, format := range sortFormats(formatDefinitionById) {
+					printFormat(formatDefinitionById[format])
+				}
+				tableWriter.Render()
+				cmd.Print(buffer.String())
+			}
+		},
+	}
 }
 
 func init() {
+	listTimeFormatsCmd := newListTimeFormatsCommand()
 	rootCmd.AddCommand(listTimeFormatsCmd)
 	listTimeFormatsCmd.PersistentFlags().Bool("sorted", true, "display the formats in sorted order. Use --sorted=true or --sorted=false")
 }
---
+++ cmd/listTimeFormats_test.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"bytes"
+	"goselect/parser/context"
+	"strings"
+	"testing"
+)
+
+func TestTimeFormatsUnsorted(t *testing.T) {
+	rootCmd.SetArgs([]string{"listTimeFormats"})
+	buffer := new(bytes.Buffer)
+	rootCmd.SetOut(buffer)
+
+	_ = rootCmd.Execute()
+	contents := buffer.String()
+
+	formats := context.SupportedFormats()
+	for formatId, definition := range formats {
+		if !strings.Contains(contents, formatId) {
+			t.Fatalf("Expected format id %v to be contained in the supported formats but was not, received %v", formatId, contents)
+		}
+		if !strings.Contains(contents, definition.Format) {
+			t.Fatalf("Expected format %v to be contained in the supported formats but was not, received %v", definition.Format, contents)
+		}
+	}
+}
+
+func TestTimeFormatsSorted(t *testing.T) {
+	rootCmd.SetArgs([]string{"listTimeFormats", "--sorted", "true"})
+	buffer := new(bytes.Buffer)
+	rootCmd.SetOut(buffer)
+
+	_ = rootCmd.Execute()
+	contents := buffer.String()
+
+	formats := context.SupportedFormats()
+	for formatId, definition := range formats {
+		if !strings.Contains(contents, formatId) {
+			t.Fatalf("Expected format id %v to be contained in the supported formats but was not, received %v", formatId, contents)
+		}
+		if !strings.Contains(contents, definition.Format) {
+			t.Fatalf("Expected format %v to be contained in the supported formats but was not, received %v", definition.Format, contents)
+		}
+	}
+}
+
+func TestTimeFormatsUnSortedWithInvalidValue(t *testing.T) {
+	rootCmd.SetArgs([]string{"listTimeFormats", "--sorted", "unknown"})
+	buffer := new(bytes.Buffer)
+	rootCmd.SetOut(buffer)
+
+	_ = rootCmd.Execute()
+	contents := buffer.String()
+
+	formats := context.SupportedFormats()
+	for formatId, definition := range formats {
+		if !strings.Contains(contents, formatId) {
+			t.Fatalf("Expected format id %v to be contained in the supported formats but was not, received %v", formatId, contents)
+		}
+		if !strings.Contains(contents, definition.Format) {
+			t.Fatalf("Expected format %v to be contained in the supported formats but was not, received %v", definition.Format, contents)
+		}
+	}
+}
