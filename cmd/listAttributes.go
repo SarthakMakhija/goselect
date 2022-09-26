9049b58dd07886011172563c83ebb76f53b3d4db
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-09-26 17:55:52 +0530

    Sarthak | Refactors listAttributes

--- cmd/listAttributes.go
+++ cmd/listAttributes.go
@@ -1,70 +1,77 @@
 package cmd
 
 import (
+	"bytes"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 	"goselect/parser/context"
-	"os"
 	"sort"
 	"strings"
 )
 
-var listAttributesCmd = &cobra.Command{
-	Use:     "listAttributes",
-	Aliases: []string{"attributes", "attrs"},
-	Short:   "List all the attributes supported by goselect",
-	Long:    `List all the attributes along with their aliases supported by goselect`,
-	Example: `
+func newListAttributesCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:     "listAttributes",
+		Aliases: []string{"attributes", "attrs"},
+		Short:   "List all the attributes supported by goselect",
+		Long:    `List all the attributes along with their aliases supported by goselect`,
+		Example: `
 1. goselect listAttributes --sorted=true
 2. goselect attributes --sorted=true
 3. goselect attrs --sorted=true
 `,
-	Run: func(cmd *cobra.Command, args []string) {
-		tableWriter := table.NewWriter()
-		tableWriter.SetOutputMirror(os.Stdout)
-		tableWriter.SetStyle(table.StyleColoredBlackOnCyanWhite)
-		tableWriter.Style().Options.SeparateColumns = true
+		Run: func(cmd *cobra.Command, args []string) {
+			buffer := new(bytes.Buffer)
 
-		asString := func(aliases []string) string {
-			return strings.Join(aliases, ", ")
-		}
-		printHeader := func() {
-			tableWriter.AppendHeader(table.Row{"Attribute", "Aliases"})
-		}
-		printAttribute := func(attribute string, aliases []string) {
-			tableWriter.AppendRow(table.Row{attribute, asString(aliases)})
-		}
-		printAttributes := func(aliasesByAttribute map[string][]string) {
-			for attribute, aliases := range aliasesByAttribute {
-				printAttribute(attribute, aliases)
-			}
-		}
-		sortAttributes := func(aliasesByAttribute map[string][]string) []string {
-			attributes := make([]string, 0, len(aliasesByAttribute))
-			for attribute := range aliasesByAttribute {
-				attributes = append(attributes, attribute)
-			}
-			sort.Strings(attributes)
-			return attributes
-		}
+			tableWriter := table.NewWriter()
+			tableWriter.SetOutputMirror(buffer)
+			tableWriter.SetStyle(table.StyleColoredBlackOnCyanWhite)
+			tableWriter.Style().Options.SeparateColumns = true
 
-		isSorted, _ := cmd.Flags().GetBool("sorted")
-		aliasesByAttribute := context.NewAttributes().AllAttributeWithAliases()
-		printHeader()
-
-		if !isSorted {
-			printAttributes(aliasesByAttribute)
-			tableWriter.Render()
-		} else {
-			for _, attribute := range sortAttributes(aliasesByAttribute) {
-				printAttribute(attribute, aliasesByAttribute[attribute])
+			asString := func(aliases []string) string {
+				return strings.Join(aliases, ", ")
 			}
-			tableWriter.Render()
-		}
-	},
+			appendHeader := func() {
+				tableWriter.AppendHeader(table.Row{"Attribute", "Aliases"})
+			}
+			appendAttribute := func(attribute string, aliases []string) {
+				tableWriter.AppendRow(table.Row{attribute, asString(aliases)})
+			}
+			appendAttributes := func(aliasesByAttribute map[string][]string) {
+				for attribute, aliases := range aliasesByAttribute {
+					appendAttribute(attribute, aliases)
+				}
+			}
+			sortAttributes := func(aliasesByAttribute map[string][]string) []string {
+				attributes := make([]string, 0, len(aliasesByAttribute))
+				for attribute := range aliasesByAttribute {
+					attributes = append(attributes, attribute)
+				}
+				sort.Strings(attributes)
+				return attributes
+			}
+
+			isSorted, _ := cmd.Flags().GetBool("sorted")
+			aliasesByAttribute := context.NewAttributes().AllAttributeWithAliases()
+			appendHeader()
+
+			if !isSorted {
+				appendAttributes(aliasesByAttribute)
+				tableWriter.Render()
+				cmd.Print(buffer.String())
+			} else {
+				for _, attribute := range sortAttributes(aliasesByAttribute) {
+					appendAttribute(attribute, aliasesByAttribute[attribute])
+				}
+				tableWriter.Render()
+				cmd.Print(buffer.String())
+			}
+		},
+	}
 }
 
 func init() {
+	listAttributesCmd := newListAttributesCommand()
 	rootCmd.AddCommand(listAttributesCmd)
 	listAttributesCmd.PersistentFlags().Bool("sorted", true, "display the attributes in sorted order. Use --sorted=true or --sorted=false")
 }
