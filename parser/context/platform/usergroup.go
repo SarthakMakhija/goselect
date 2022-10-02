80dfcbacebf5baba4e76d728fb6b81e34cdf7de5
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-10-02 15:57:26 +0530

    Sarthak | Refactors FileAttributes, extracts user and group information in usergroup

---
+++ parser/context/platform/usergroup.go
@@ -0,0 +1,31 @@
+//go:build !windows
+// +build !windows
+
+package platform
+
+import (
+	"io/fs"
+	"os/user"
+	"strconv"
+	"syscall"
+)
+
+type UserId = string
+type UserName = string
+type GroupId = string
+type GroupName = string
+
+func UserGroup(file fs.FileInfo) (UserId, UserName, GroupId, GroupName) {
+	stat := file.Sys().(*syscall.Stat_t)
+	userId := strconv.FormatUint(uint64(stat.Uid), 10)
+
+	lookedUpUser, err := user.LookupId(userId)
+	if err != nil {
+		return "", "", "", ""
+	}
+	group, err := user.LookupGroupId(lookedUpUser.Gid)
+	if err != nil {
+		return "", "", "", ""
+	}
+	return lookedUpUser.Uid, lookedUpUser.Username, lookedUpUser.Gid, group.Name
+}
